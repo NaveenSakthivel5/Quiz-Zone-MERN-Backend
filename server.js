a0b7a9d7087d4f6f47d5ac2e33cc1f9f8e74c1a9
NaveenSakthivel5 <naveensakthivel14@gmail.com>
2024-06-24 19:27:13 +0530

    Commit message

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require('cors'); 
+require("dotenv").config();
+app.use(express.json());
+
+app.use(cors());
+
+const dbConfig = require("./config/dbConfig");
+
+const usersRoute = require("./routes/usersRoute");
+const examsRoute = require("./routes/examsRoute");
+const resportsRoute = require("./routes/reportsRoute");
+
+
+app.use("/api/users", usersRoute);
+app.use("/api/exams", examsRoute);
+app.use("/api/reports", resportsRoute);
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
